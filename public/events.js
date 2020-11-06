49f5624832188cfd4bdd0a70e63f22db5092ee73
Barry Mattern <bdmattern@gmail.com>
2020-11-06 16:33:47 -0700

    - Refactor, functions at top, listeners at bottom - Add vote(), handle upvotes & downvotes

--- public/events.js
+++ public/events.js
@@ -1,23 +1,41 @@
-window.addEventListener('DOMContentLoaded', event => {
+let getCatPic = async () => {
   let catPic = document.querySelector('.cat-pic');
-  const newPicBtn = document.querySelector('#new-pic');
-  const loader = document.querySelector('.loader');
+  let catRequest = await fetch('/kitten/image');
+  
+  if (!catRequest.ok) {
+    alert('Something went wrong! Please try again!');
+  } else {
+    const loader = document.querySelector('.loader');
+    loader.innerHTML = "Loading...";
+    let catReturned = await catRequest.json();
+    loader.innerHTML = '';
+    catPic.src = catReturned.src;
+  }
+};
 
-  let getCatPic2 = async () => {
-      let catRequest = await fetch('/kitten/image');
-      if (!catRequest.ok) {
-        alert('Something went wrong! Please try again!');
-      } else {
-        loader.innerHTML = "Loading...";
-        let catReturned = await catRequest.json();
-        loader.innerHTML = '';
-        catPic.src = catReturned.src;
-      }
-  };
+let vote = async event => {
+  try {
+    const res = await fetch(`kitten/${event.target.id}`, { method: 'PATCH' });
 
-  newPicBtn.addEventListener('click', event => {
-    getCatPic2();
-  });
+    if (!res.ok) {
+      throw res;
+    } else {
+      const data = await res.json();
+      const score = document.querySelector('.score');
+      score.innerText = data.score;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-  getCatPic2();
-});
+window.addEventListener('DOMContentLoaded', getCatPic);
+
+const newPicBtn = document.querySelector('#new-pic');
+newPicBtn.addEventListener('click', getCatPic);
+
+const upVoteBtn = document.querySelector('#upvote');
+upVoteBtn.addEventListener('click', vote);
+
+const downVoteBtn = document.querySelector('#downvote')
+downVoteBtn.addEventListener('click', vote);

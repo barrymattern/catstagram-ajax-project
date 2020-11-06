7750d7ad1b5b0d3b24e82a959d49a95e0a02518b
Barry Mattern <bdmattern@gmail.com>
2020-11-06 12:04:29 -0700

    Add new pic button functionality

--- public/events.js
+++ public/events.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', event => {
   let catPic = document.querySelector('.cat-pic');
+  const newPicBtn = document.querySelector('#new-pic');
+  const loader = document.querySelector('.loader');
 
   // function getCatPic() {
   //   fetch('/kitten/image')
@@ -11,11 +13,16 @@ window.addEventListener('DOMContentLoaded', event => {
 
   let getCatPic2 = async () => {
     let catRequest = await fetch('/kitten/image');
+    loader.innerHTML = "Loading...";
     let catReturned = await catRequest.json();
     let catObj = await catReturned;
-
+    loader.innerHTML = '';
     catPic.src = catObj.src;
   };
+
+  newPicBtn.addEventListener('click', event => {
+    getCatPic2();
+  });
 
   getCatPic2();
 });

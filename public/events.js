e019da4485d64d6b082451c0ff36d0636cdc3002
Barry Mattern <bdmattern@gmail.com>
2020-11-08 18:03:08 -0700

    Solve Phase 2: Give newPicBtn Functionality

--- public/events.js
+++ public/events.js
@@ -1,8 +1,19 @@
+const updateLoadStatus = () => {
+  const loadStatus = document.querySelector('.loader');
+  if (!loadStatus.innerHTML) {
+    loadStatus.innerHTML = 'Loading...';
+  } else {
+    loadStatus.innerHTML = '';
+  }
+};
+
 const getCatPic = async () => {
   try {
     const catPic = document.querySelector('.cat-pic');
     const catPicRequest = await fetch('/kitten/image');
+    updateLoadStatus();
     const data = await catPicRequest.json();
+    updateLoadStatus();
 
     catPic.src = data.src;
   } catch (e) {
@@ -11,3 +22,6 @@ const getCatPic = async () => {
 };
 
 window.addEventListener('DOMContentLoaded', getCatPic);
+
+const newPicBtn = document.querySelector('#new-pic');
+newPicBtn.addEventListener('click', getCatPic);
